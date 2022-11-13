ec4bc6f87266541bf6a21d1355e118716aeee6c8
Bin-08-01 <minhtri192035@gmail.com>
2022-11-13 21:29:51 +0700

    🚀Add schedule features

---
+++ Controllers/ScheduleController.js
@@ -0,0 +1,102 @@
+const ScheduleModel = require("../Models/Schedule");
+const ProjectModel = require("../Models/Project");
+const jwt = require("jsonwebtoken");
+
+const ScheduleModel = {
+    getAll: async (req, res) => {
+        try{
+            const userID = jwt.decode(req.headers.token.split(" ")[1]).id;
+            const idProject = req.params.idProject;
+            const checkProject = await ProjectModel.findById(idProject);
+
+            if(idProject && userID && checkProject){
+                const schedule = await ScheduleModel.find({idProject: idProject});
+                res.status(200).json(schedule);
+            }else{
+                res.status(403).json({message: "Yêu cầu không hợp lệ"});
+            }
+        }catch(err){
+            res.status(500).json("Lỗi");
+        }
+    },
+
+    getDetail: async (req, res) => {
+        try{
+            const userID = jwt.decode(req.headers.token.split(" ")[1]).id;
+            const idSchedule = req.params.idSchedule;
+            const schedule = await ScheduleModel.findById(idSchedule);
+            if(schedule){
+                res.status(200).json(schedule);
+            }else{
+                res.status(403).json({message: "Yêu cầu không hợp lệ"});
+            }
+        }catch(err){
+            res.status(500).json("Lỗi");
+        }
+    },
+
+    addSchedule: async (req, res) => {
+        try{
+            const userID = jwt.decode(req.headers.token.split(" ")[1]).id;
+            const idProject = req.params.idProject;
+            const checkProject = await ProjectModel.findById(idProject);
+            if(idProject && userID && checkProject) {
+                const schedule = await ScheduleModel({
+                    idProject: idProject,
+                    title: req.body.title,
+                    description: req.body.description,
+                    state: req.body.state,
+                    stateAlarm: req.body.stateAlarm,
+                    dayStart: req.body.dayStart,
+                    dayEnd: req.body.dayEnd
+                });
+                await schedule.save();
+                res.status(200).json({message: "Thêm schedule thành công"});
+            }else{
+                res.status(403).json({message: "Yêu cầu không hơp lệ"});
+            }
+        }catch(err){
+            res.status(500).json("Lỗi");
+        }
+    },
+
+    deleteSchedule: async (req, res) => {
+        try{
+            const userID = jwt.decode(req.headers.token.split(" ")[1]).id;
+            const idSchedule = req.params.idSchedule;
+            const checkSchedule = await ScheduleModel.findById(idSchedule);
+            if(userID && checkSchedule){
+                await ScheduleModel.findByIdAndDelete(idSchedule);
+                res.status(200).json({message: "Đã xoá schedule"});
+            }else{
+                res.status(403).json({message: "Yêu cầu không hợp lệ"});
+            }
+        }catch(err){
+            res.status(500).json("Lỗi");
+        }
+    },
+
+    editSchedule: async (req, res) => {
+        try{
+            const userID = jwt.decode(req.headers.token.split(" ")[1]).id;
+            const idSchedule = req.params.idSchedule;
+            const checkSchedule = await ScheduleModel.findById(idSchedule);
+            if(userID && checkSchedule){
+                checkSchedule.title = req.body.title;
+                checkSchedule.description = req.body.description;
+                checkSchedule.stateAlarm = req.body.stateAlarm;
+                checkSchedule.dayStart = req.body.dayStart;
+                checkSchedule.dayEnd = req.body.dayEnd;
+                checkTodo.state = req.body.state;
+                await checkSchedule.save();
+                res.status(200).json({message: "Sửa schedule thành công"});
+            }else{
+                res.status(403).json({message: "Yêu cầu không hợp lệ"});
+            }
+        }catch(err){
+            res.status(500).json("Lỗi");
+        }
+    }
+}
+
+module.exports = ScheduleModel;
