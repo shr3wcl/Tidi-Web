412a551588be6debde3446701b57d96009fcfe4a
Bin-08-01 <minhtri192035@gmail.com>
2022-12-05 18:57:02 +0700

    Update FollowController.js

--- Controllers/FollowController.js
+++ Controllers/FollowController.js
@@ -1,11 +1,13 @@
 const FollowModel = require("../Models/Follow");
 const { find } = require("../Models/Project");
+const jwt = require("jsonwebtoken");
 
 const FollowController = {
     getAllFollow: async (req, res) => {
         try {
             const idUser = req.params.idUser;
-            const data = await FollowModel.find({ idUser: idUser }).select("followers").sort([['updatedAt', -1]]);
+            const data = await FollowModel.find({ idUser: idUser }).select('idFollow').sort([['updatedAt', -1]]).populate('idFollow', 'avatar firstName lastName');
+            console.log(data);
             return res.status(200).json(data);
         } catch (error) {
             res.status(500).json("Error");
@@ -16,10 +18,15 @@ const FollowController = {
         try {
             const userID = jwt.decode(req.headers.token.split(" ")[1]).id ?? false;
             if (userID) {
-                const listFollower = await FollowModel.findById(req.params.idUser);
-                listFollower.followers = [listFollower.followers, { userID }];
+                const newFollow = await FollowModel({
+                    idUser: userID,
+                    idFollow: req.body.idFollow
+                });
+                await newFollow.save();
+                res.status(200).json("Successful");
             }
         } catch (error) {
+            console.log(error);
             res.status(500).json("Error");
         }
     },
@@ -28,8 +35,20 @@ const FollowController = {
         try {
             const userID = jwt.decode(req.headers.token.split(" ")[1]).id;
             if (userID) {
-                const listFollower = await FollowModel.findById(req.params.idUser);
-                listFollower.followers = [listFollower.followers, { userID }];
+                await FollowModel.findOneAndDelete({ idUser: userID, idFollow: req.body.idFollow });
+                return res.status(200).json("Successful");
+            }
+        } catch (error) {
+            res.status(500).json("Error");
+        }
+    },
+
+    checkFollow: async (req, res) => {
+        try {
+            const userID = jwt.decode(req.headers.token.split(" ")[1]).id;
+            if (userID) {
+                const data = await FollowModel.findOne({ idUser: userID, idFollow: req.body.idFollow });
+                return res.status(200).json(data);
             }
         } catch (error) {
             res.status(500).json("Error");
