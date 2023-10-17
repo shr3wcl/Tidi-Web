5466680c19c92703b9198011b66ea9af02af61a4
Phạm Minh Trí <minhtri192035@gmail.com>
2023-10-17 21:42:20 +0700

    Update

--- frontend/src/Component/blog/header/Share.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Share = () => {
-    return(
-        <div></div>
-    )
-}
-
-export default Share;
--- frontend/src/Component/profile/showInfo/ShowInfo.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const ShowInfo = () => {
-    return(
-        <div>Hello</div>
-    )
-}
-
-export default ShowInfo;
